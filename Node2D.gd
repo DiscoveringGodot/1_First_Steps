02a002588f2a03b4adac231ac930c6cbb631e4d9
YannBurrett <37750928+YannBurrett@users.noreply.github.com>
2018-04-21 16:03:57 -0400

    Delete Node2D.gd

--- Node2D.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var story
-var player_words = ["Yann", "coffee shop", "cake", "sick"]
-onready var StoryText = $Blackboard/StoryText
-
-func _ready():
-	story = ("Once upon a time, a %s lived happily ever after in a %s.  They ate a bunch of %s, and all felt very %s." )
-	StoryText.bbcode_text = story % player_words
